c71d9b85c7e2e44887ac0cc5be764c1cf23be99f
Veeno <77930262+Veenoway@users.noreply.github.com>
2022-04-28 13:30:12 +0200

    Debug some problem

---
+++ src/components/CoinData.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Veeno from "../img/blanc-bleu.png"
+
+const CoinData = () => {
+    return(
+        <div className="d-flex justify-content-center align-items-center">
+            <img src={Veeno} width="120"/>
+            <h2>CoinData</h2>
+        </div>
+    )
+}
+
+export default CoinData;
