0025e64200fdb503ec8115d8c158ec04854abf71
Veeno <77930262+Veenoway@users.noreply.github.com>
2022-04-28 07:15:44 +0200

    HomePage Done

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link} from "react-router-dom";
 
-const Coin = ({coin}) => {
+const Coin = ({coin, deleteCoin}) => {
     return(
-        <Link to="/coinDetail">
-            <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
+        <Link  className="text-decoration-none my-1 coin">
+            <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark rounded">
                 <img src={coin.image} className="coinlist-image"/>
+                <span className="text-decoration-none">{coin.name}</span>
                 <span className="text-decoration-none">{coin.current_price} $</span>
-                <span className="text-success mr-2">
-                    <i className="fas fa-sort-down align-middle mr-1"></i>
+                <span className={coin.price_change_percentage_24h < 0 ? "text-danger mright" : "text-success mright"} >
+                    { " " }
+                    {coin.price_change_percentage_24h < 0 ? (
+                        <i className="fas fa-sort-down align-middle mx-2 mb-1"></i>
+                    ) : (
+                        <i className="fas fa-sort-up align-middle mx-2 mt-1"></i>
+                    )}
                     {coin.price_change_percentage_24h} %
                 </span>
-                <i className="delete-icon far fa-times-circle text-danger"></i>
+                <i onClick={(e) => {
+                    deleteCoin(coin.id)
+                }} className="delete-icon far fa-times-circle text-danger" id="delete"></i>
             </li>
         </Link>
     )
--- src/pages/CoinSummaryPage.jsx
+++ src/pages/CoinSummaryPage.jsx
@@ -4,7 +4,8 @@ import AddCoin from '../components/AddCoin';
 
 const CoinSummaryPage = () => {
     return(
-        <div>
+        <div className=" coinsummary pt-4 pb-5">
+            <h2 className="text-center pb- text-light">Crypto Charts :</h2>
             <AddCoin />
             <CoinList />
         </div>
